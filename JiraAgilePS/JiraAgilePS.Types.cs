45ad91b35c27dd03004d321cf638d02758ff27be
Oliver Lipkau <oliver@lipkau.net>
2018-05-27 22:40:31 +0200

    Added first draft of classes

--- JiraAgilePS/JiraAgilePS.Types.cs
+++ JiraAgilePS/JiraAgilePS.Types.cs
@@ -3,97 +3,54 @@ using System.Collections.Generic;
 using System.Collections;
 // using System.Linq;
 
-namespace ConfluencePS
+namespace AtlassianPS
 {
 
-	public class Icon {
-		public String Path { get; set; }
-		public Int32 Width { get; set; }
-		public Int32 Height { get; set; }
-		public Boolean IsDefault { get; set; }
-		public override string ToString() {
-			return Path;
-		}
-	}
+    namespace JiraPS
+    {
 
-	public class User {
-		public String UserName { get; set; }
-		public String DisplayName { get; set; }
-		public String UserKey { get; set; }
-		public Icon ProfilePicture { get; set; }
-		public override string ToString() {
-			return UserName;
-		}
-	}
+        public enum ProjectTypeKey
+        {
+            // TODO
+            "software"
+        }
 
-	public class Version {
-		public User By { get; set; }
-		public DateTime When { get; set; }
-		public String FriendlyWhen { get; set; }
-		public Int32 Number { get; set; }
-		public String Message { get; set; }
-		public Boolean MinorEdit { get; set; }
-		public override string ToString() {
-			return Number.ToString();
-		}
-	}
+        namespace Agile
+        {
 
-	public class Space {
-		public Int32 Id { get; set; }
-		public String Key { get; set; }
-		public String Name { get; set; }
-		public Icon Icon { get; set; }
-		public String Type { get; set; }
-		public String Description { get; set; }
-		public Page Homepage { get; set; }
-		public override string ToString() {
-			return "[" + Key + "] " + Name;
-		}
-	}
+            using AtlassianPS.JiraPS;
 
-	public class Page {
-		public Int32 ID { get; set; }
-		public String Status { get; set; }
-		public String Title { get; set; }
-		public Space Space { get; set; }
-		public Version Version { get; set; }
-		public String Body { get; set; }
-		public Page[] Ancestors { get; set; }
-		public String URL { get; set; }
-		public String ShortURL { get; set; }
-		public override string ToString() {
-			return "[" + ID + "] " + Title;
-		}
-	}
+            public enum BoardType
+            {
+                "scrum"
+                "kanban"
+            }
 
-	public class Label {
-		public Int32 ID { get; set; }
-		public String Prefix { get; set; }
-		public String Name { get; set; }
-		public override string ToString() {
-			return Name;
-		}
-	}
+            public class BoardLocation
+            {
+                public Int32 ProjectId { get; set; }
+                public String Name { get; set; }
+                public ..\ProjectTypeKey ProjectTypeKey { get; set; }
+                public String avatarURI { get; set; }
+                public override string ToString() {
+                    return Name;
+                }
+            }
 
-	public class ContentLabelSet {
-		public Page Page { get; set; }
-		public Label[] Labels { get; set; }
-	}
+            public class Board
+            {
+                public Int32 Id { get; set; }
+                public String Name { get; set; }
+                public BoardType Type { get; set; }
+                public BoardLocation Location { get; set; }
+                public String RestURL { get; set; }
+                public override string ToString() {
+                    return Name;
+                }
+            }
 
-	public class Attachment {
-		public Int32 ID { get; set; }
-		public String Status { get; set; }
-		public String Title { get; set; }
-		public String Filename { get; set; }
-		public String MediaType { get; set; }
-		public Int32 FileSize { get; set; }
-		public String Comment { get; set; }
-		public String SpaceKey { get; set; }
-		public Int32 PageID { get; set; }
-		public Version Version { get; set; }
-		public String URL { get; set; }
-		public override string ToString() {
-			return "[att$ID] $Title";
-		}
-	}
+        }
+
+    }
+
 }
